8694284b2c5b943b3d6269444cdf7d82ce377e86
Tim Fabritius <107303668+tcfabritius@users.noreply.github.com>
2024-11-18 10:14:13 +0200

    Add files via upload

---
+++ backend/flask_app.py
@@ -0,0 +1,403 @@
+from flask import Flask, Response, request
+from dbconf import db
+from flask_cors import CORS
+import json
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.after_request
+def apply_cors(response):
+    if request.environ.get('HTTP_ORIGIN'):
+        response.headers['Access-Control-Allow-Origin'] = request.environ['HTTP_ORIGIN']
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/kentat')
+def kentat():
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = "select * from airport"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        columns = [
+            "id", "ident", "type", "name", "latitude_deg", "longitude_deg",
+            "elevation_ft", "continent", "iso_country", "iso_region", "municipality",
+            "scheduled_service", "gps_code", "iata_code", "local_code", "home_link",
+            "wikipedia_link", "keywords"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "airports": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+
+@app.route('/kentta/<icao>')
+def kentta(icao):
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = f"select * from airport where ident = '{icao}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        if not tulos:
+            tilakoodi = 404
+            vastaus = {"status": tilakoodi, "teksti": "Ei tuloksia"}
+            return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+        vastaus = {
+            "status": tilakoodi,
+            "id": tulos[0][0],
+            "ident": tulos[0][1],
+            "type": tulos[0][2],
+            "name": tulos[0][3],
+            "latitude_deg": tulos[0][4],
+            "longitude_deg": tulos[0][5],
+            "elevation_ft": tulos[0][6],
+            "continent": tulos[0][7],
+            "iso_country": tulos[0][8],
+            "iso_region": tulos[0][9],
+            "municipality": tulos[0][10],
+            "scheduled_service": tulos[0][11],
+            "gps_code": tulos[0][12],
+            "iata_code": tulos[0][13],
+            "local_code": tulos[0][14],
+            "home_link": tulos[0][15],
+            "wikipedia_link": tulos[0][16],
+            "keywords": tulos[0][17]
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/maat')
+def maat():
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = "select * from country"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        columns = [
+            "iso_country", "name", "continent", "wikipedia_link", "keywords"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "countries": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/maa/<countrycode>')
+def maa(countrycode):
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = f"select * from country where iso_country = '{countrycode}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        if not tulos:
+            tilakoodi = 404
+            vastaus = {"status": tilakoodi, "teksti": "Ei tuloksia"}
+            return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+        vastaus = {
+            "status": tilakoodi,
+            "name": tulos[0][0],
+            "continent": tulos[0][1],
+            "wikipedia_link": tulos[0][2],
+            "keywords": tulos[0][3]
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/pelaajat')
+def pelaajat():
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = "select * from game"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        columns = [
+            "id", "co2_consumed", "co2_budget", "location", "money",
+            "threat"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "players": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/pelaaja/<nimi>')
+def pelaaja(nimi):
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = f"select * from game where id = '{nimi}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        if not tulos:
+            tilakoodi = 404
+            vastaus = {"status": tilakoodi, "teksti": "Ei tuloksia"}
+            return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+        vastaus = {
+            "status": tilakoodi,
+            "id": tulos[0][0],
+            "co2_consumed": tulos[0][1],
+            "co2_budget": tulos[0][2],
+            "location": tulos[0][3],
+            "money": tulos[0][4],
+            "threat": tulos[0][5]
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/suoritustilanne')
+def suoritustilanne():
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = "select * from mission_accomplished"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        columns = [
+            "game_id", "mission_id"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "status": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/pelaajantilanne/<nimi>')
+def pelaajantilanne(nimi):
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = f"select * from mission_accomplished where game_id = '{nimi}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        if not tulos:
+            tilakoodi = 404
+            vastaus = {"status": tilakoodi, "teksti": "Ei tuloksia"}
+            return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+        columns = [
+            "game_id", "mission_id"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "status": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/tehtavat')
+def tehtavat():
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = "select * from mission"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        columns = [
+            "id", "description", "pay"
+        ]
+        data = [dict(zip(columns, row)) for row in tulos]
+        vastaus = {
+            "status": tilakoodi,
+            "missions": data
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.route('/tehtava/<nro>')
+def tehtava(nro):
+    yhteys = db()
+    if not yhteys.is_connected():
+        return Response(response=json.dumps({"status": 500, "teksti": "Tietokantayhteys epäonnistui"}), status=500, mimetype="application/json")
+    tilakoodi = 200
+    try:
+        sql_kysely = f"select * from mission where id = '{nro}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_kysely)
+        tulos = kursori.fetchall()
+        if not tulos:
+            tilakoodi = 404
+            vastaus = {"status": tilakoodi, "teksti": "Ei tuloksia"}
+            return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+        vastaus = {
+            "status": tilakoodi,
+            "id": tulos[0][0],
+            "description": tulos[0][1],
+            "pay": tulos[0][2]
+        }
+        jsonvast = json.dumps(vastaus)
+        return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+    except Exception as e:
+        tilakoodi = 500
+        vastaus = {"status": tilakoodi, "error": str(e)}
+        return Response(response=json.dumps(vastaus), status=tilakoodi, mimetype="application/json")
+
+    finally:
+        if 'kursori' in locals():
+            kursori.close()
+        if 'yhteys' in locals() and yhteys.is_connected():
+            yhteys.close()
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus = {
+        "status" : "404",
+        "teksti" : "Virheellinen päätepiste"
+    }
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=404, mimetype="application/json")
---
+++ backend/tietok.sql
@@ -0,0 +1,129 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.6.19-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: flight_game
+-- ------------------------------------------------------
+-- Server version	10.6.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `airport`
+--
+
+DROP TABLE IF EXISTS `airport`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `airport` (
+  `id` int(11) NOT NULL,
+  `ident` varchar(40) NOT NULL,
+  `type` varchar(40) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `latitude_deg` double DEFAULT NULL,
+  `longitude_deg` double DEFAULT NULL,
+  `elevation_ft` int(11) DEFAULT NULL,
+  `continent` varchar(40) DEFAULT NULL,
+  `iso_country` varchar(40) DEFAULT NULL,
+  `iso_region` varchar(40) DEFAULT NULL,
+  `municipality` varchar(40) DEFAULT NULL,
+  `scheduled_service` varchar(40) DEFAULT NULL,
+  `gps_code` varchar(40) DEFAULT NULL,
+  `iata_code` varchar(40) DEFAULT NULL,
+  `local_code` varchar(40) DEFAULT NULL,
+  `home_link` varchar(40) DEFAULT NULL,
+  `wikipedia_link` varchar(40) DEFAULT NULL,
+  `keywords` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`ident`),
+  KEY `iso_country` (`iso_country`),
+  CONSTRAINT `airport_ibfk_1` FOREIGN KEY (`iso_country`) REFERENCES `country` (`iso_country`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `iso_country` varchar(40) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `continent` varchar(40) DEFAULT NULL,
+  `wikipedia_link` varchar(40) DEFAULT NULL,
+  `keywords` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`iso_country`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `id` varchar(40) NOT NULL,
+  `co2_consumed` int(8) DEFAULT NULL,
+  `co2_budget` int(8) DEFAULT NULL,
+  `location` varchar(10) DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `threat` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `location` (`location`),
+  CONSTRAINT `game_ibfk_1` FOREIGN KEY (`location`) REFERENCES `airport` (`ident`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mission`
+--
+
+DROP TABLE IF EXISTS `mission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mission` (
+  `id` int(11) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `pay` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mission_accomplished`
+--
+
+DROP TABLE IF EXISTS `mission_accomplished`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mission_accomplished` (
+  `game_id` varchar(40) NOT NULL,
+  `mission_id` int(11) NOT NULL,
+  PRIMARY KEY (`game_id`,`mission_id`),
+  KEY `goalid` (`mission_id`),
+  CONSTRAINT `mission_accomplished_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  CONSTRAINT `mission_accomplished_ibfk_2` FOREIGN KEY (`mission_id`) REFERENCES `mission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-04 13:14:14
