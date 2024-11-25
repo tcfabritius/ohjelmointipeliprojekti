dfbc8e27ae6ba880501216f1a84d3f2b2d378163
skm1900 <sv.k.mattila@saunalahti.fi>
2024-11-25 23:12:02 +0200

    html_version_1.03

--- frontend/hackerGame.js
+++ frontend/hackerGame.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const locQuery = document.querySelector("#icao");
+
+const locQuery = document.querySelector("#icaoSearch");
 locQuery.addEventListener("submit", async function(evt) {
     evt.preventDefault();
     // get value of input element
     const criteria = document.getElementById("icao").value;
-    console.log(criteria);
+    //console.log(criteria);
     try {
         const response = await fetch("https://timfabritius1.pythonanywhere.com/kentta/"+criteria);
         const jsonData = await response.json();
-        console.log(criteria, jsonData);
+        //console.log(criteria, jsonData);
+        map.setView(new L.LatLng(jsonData.latitude_deg, jsonData.longitude_deg), 12);
+        document.getElementById("icao").value = "";
     } catch (error) {
         console.log(error.message);
     }
